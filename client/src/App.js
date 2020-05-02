45782a3e28463b3bd9a4be97f0e82bd7fe8b1df5
ArtemBaskal <a.baskal@adguard.com>
2020-05-03 01:24:18 +0300

    Setup microservices

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PostCreate from "./PostCreate";
+import PostLists from "./PostList";
+
+export default () => {
+    return <div className="container">
+        <h1>Create Post</h1>
+        <PostCreate />
+        <hr />
+        <h1>Posts</h1><PostLists />
+    </div>
+}
---
+++ client/src/CommentCreate.js
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+import axios from 'axios';
+
+export default ({postId}) => {
+    const [content, setContent] = useState('');
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        await axios.post(`http://posts.com/posts/${postId}/comments`, {
+            content
+        });
+
+        setContent('');
+    };
+
+    return <div>
+        <form onSubmit={onSubmit}>
+            <div className="form-group">
+                <label>New comment</label>
+                <input
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                    className="form-control"
+                />
+            </div>
+            <button className="btn btn-primary">Submit</button>
+        </form>
+    </div>
+}
